104c0c12ff4e6c64cee7fc7ec27b1b79ad4f6369
Ejayz <32452006+Ejayz@users.noreply.github.com>
2025-10-08 11:55:30 +0800

    Restructure app layout and add directory search
    Moved layout.tsx to app/(index)/layout.tsx and updated font usage. Replaced app/page.tsx with app/(index)/page.tsx, introducing a new DirectorySearch component. Added components/Directory.tsx for search input. Integrated daisyui into Tailwind config and updated postcss config. Added react-query, daisyui, formik, react-toastify, and yup dependencies.

---
+++ app/(index)/page.tsx
@@ -0,0 +1,22 @@
+import DirectorySearch from "@/components/Directory";
+
+export default function Home() {
+  return (
+    <div className="font-mono flex flex-col items-center justify-items-center min-h-screen">
+      <main className="flex flex-col  items-center min-h-screen sm:items-start">
+      <DirectorySearch>
+
+      </DirectorySearch>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Having a problem ? Contact us 3907 Net Ad Team
+        </a>
+      </footer>
+    </div>
+  );
+}
